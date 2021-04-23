44ea21746ed3167e51ae9d0c9fd7200cfc8e670e
priyankakbari123 <82721030+priyankakbari123@users.noreply.github.com>
2021-04-23 19:29:36 +0530

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,12 @@
+#1
+class counter:
+    count=0
+    def __init__(self):
+        self.count=0
+    def countUp(self):
+        self.count+=1
+c=counter()
+c.countUp()
+print(c.count)
+c.countUp()
+print(c.count)
